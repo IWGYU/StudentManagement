e167b90aab904a45b0e27c8c994d552405932b86
DuckTheMug <duc.12.2002@gmail.com>
2023-11-20 19:55:26 +0700

    initial commit

--- certification.py
+++ certification.py
@@ -1,12 +1,14 @@
 class Certification:
     #constructor
     def __init__(self, cerType: str, score: float):
+        cerType = self.__parseCerType(cerType)
         self.__checkCerType(cerType)
         self.__checkScore(score)
         self.__cerType = cerType
         self.__score = self.__pasreScore(score)
     #getter & setter
     def setCerType(self, cerType: str):
+        cerType = self.__parseCerType
         self.__checkCerType(cerType)
         self.__cerType = cerType
     def setScore(self, score: float):
@@ -18,12 +20,14 @@ class Certification:
         return self.__score
     #validator
     def __checkCerType(cerType: str):
-        if (cerType.lower() != 'toeic') | (cerType.lower() != 'ielts'):
+        if (cerType != 'toeic') | (cerType != 'ielts'):
             raise ValueError
     def __checkScore(self, score: float):
-        if ((self.__cerType.lower() == 'ielts') & ((score < 0.0) | (score > 9.0))) | ((self.__cerType.lower() == 'toeic') & ((score < 0.0) | (score > 990.0) | (score % 10 != 0))):
+        if ((self.__cerType == 'ielts') & ((score < 0.0) | (score > 9.0))) | ((self.__cerType == 'toeic') & ((score < 0.0) | (score > 990.0) | (score % 5 != 0))):
             raise ValueError
     def __pasreScore(self, score: float) -> float:
-        if self.__cerType.lower() == 'toeic':
+        if self.__cerType == 'toeic':
             score = score / 100
         return score
+    def __parseCerType(cerType: str) -> str:
+        return cerType.strip().lower()
