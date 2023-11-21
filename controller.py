458f3e2a7f02df6fb4f0055e574a71ffcb77b490
Minht00 <luongtuanminh02161@gmail.com>
2023-11-21 09:54:59 +0700

    update

--- controller.py
+++
@@ -1,9 +0,0 @@
-from student import Student
-
-class ManageStudent:
-    def __init__(self):
-        self.__lst = []
-    def addStudent(self, student: Student):
-        self.__lst.append(student)
-    def getStudentsAsList(self) -> list:
-        return self.__lst
